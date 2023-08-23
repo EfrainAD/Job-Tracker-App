107e6ac710ad842468dff2becb3bd301e3303f5d
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-08-23 18:41:25 -0400

    moved testPage to be home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,10 @@
 import logo from '../logo.svg'
-const TestPage = () => {
+
+const Home = () => {
    return (
       <div className="App">
          <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
             <a
                className="App-link"
                href="https://reactjs.org"
@@ -20,4 +18,4 @@ const TestPage = () => {
    )
 }
 
-export default TestPage
+export default Home
