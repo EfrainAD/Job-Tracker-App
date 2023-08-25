be69ca729e4a3ec6b133e00f4d177c4bfc949756
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-08-24 20:54:19 -0400

    feature: Added toaster to display messages to the user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import PageRouter from './router/pageRouter'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
-const App = () => <PageRouter />
+function App() {
+   return (
+      <>
+         <ToastContainer />
+         <PageRouter />
+      </>
+   )
+}
 
 export default App
