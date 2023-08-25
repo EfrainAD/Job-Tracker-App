ba99d620099161d31eb11bdfc20256dec99bb9e7
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-08-25 00:46:41 -0400

    feature: Set up a way to restrict pages to only logged in users AND set it on home page for testing

---
+++ src/router/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectIsLoggedIn } from '../redux/user/user.selector'
+
+const ProtectedPage = ({ children }) => {
+   const isLoggedIn = useSelector(selectIsLoggedIn)
+
+   if (!isLoggedIn) {
+      return <Navigate to="/login" />
+   } else {
+      return children
+   }
+}
+
+export default ProtectedPage
--- src/router/pageRouter.js
+++ src/router/pageRouter.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home'
 import Login from '../pages/auth/login'
+import ProtectedPage from './ProtectedRoute'
 
 const PageRouter = () => {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signin" element={<Login />} />
+            <Route
+               path="/"
+               element={
+                  <ProtectedPage>
+                     <Home />
+                  </ProtectedPage>
+               }
+            />
+            <Route path="/login" element={<Login />} />
          </Routes>
       </BrowserRouter>
    )
