833c4a4bf42dfbeff6147780136fd60ea4dda5ea
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-09-02 12:56:27 -0400

    feature: Added error to the job fetch, AND user is logged out if they get a 401 error

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -2,14 +2,31 @@ import JobList from '../../components/job/jobList/JobList'
 import JobSummery from '../../components/job/jobSummery/jobSummery'
 
 import { useGetJobsQuery } from '../../api/apiSlice'
+import { useDispatch } from 'react-redux'
+import { setUserStatus } from '../../redux/user/user.action'
+import { toast } from 'react-toastify'
 
 const Dashboard = () => {
-   const { data: jobs, isLoading: isJobLoading } = useGetJobsQuery()
+   const dispatch = useDispatch()
+   const {
+      data: jobs,
+      error: isJobError,
+      isLoading: isJobLoading,
+   } = useGetJobsQuery()
+
+   if (isJobError?.status === 401) {
+      dispatch(setUserStatus(false))
+      toast.error("You're not logged in.")
+   }
 
    return (
       <>
          <JobSummery jobs={jobs} />
-         <JobList jobs={jobs} isloading={isJobLoading} />
+         <JobList
+            jobs={jobs}
+            isJobError={isJobError}
+            isloading={isJobLoading}
+         />
       </>
    )
 }
