6a04beafccafdd363827985aed370a24f9f92c68
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-09-08 00:00:36 -0400

    dev: Set form to have it's field filled

--- src/pages/job/addJob/AddJob.js
+++ src/pages/job/addJob/AddJob.js
@@ -7,9 +7,25 @@ import { useNavigate } from 'react-router-dom'
 
 const AddJob = () => {
    const [job, setJob] = useState({
-      jobTitle: 'hi',
       companyName: 'hi2',
+      companySize: '234',
+      coverLetter: 'http://localhost:3000/dashboard/add-job',
       dateApplied: '2023-09-07',
+      easyApply: true,
+      firstInterviewDate: '2023-09-21',
+      jobLocation: 'myadct',
+      jobSource: 'LinkedIn',
+      jobTitle: 'hi',
+      jobURL:
+         'https://www.linkedin.com/jobs/view/3446422410/?alternateChannel=search&refId=h8%2BDzac6dF9Rwo0Vsk821A%3D%3D&trackingId=oGJGoelPr0IsXpU8qDU7zg%3D%3D&trk=d_flagship3_postapply_immediate_modal_add_skill',
+      jobalyticsRating: '22',
+      rejectionDate: '2023-09-14',
+      rejectionReason: 'I hope \nthis \nworks!',
+      remote: 'on-site',
+      requiredExperience: '3',
+      resume: 'http://localhost:3000/dashboard/add-job',
+      secondInterviewDate: '2023-10-06',
+      technicalChallengeInterviewDate: '2023-09-28',
    })
    const [postJob, { isLoading, isError, error, isSuccess, data }] =
       useSaveJobMutation()
@@ -46,7 +62,7 @@ const AddJob = () => {
 
    const saveJob = async (e) => {
       e.preventDefault()
-
+      console.log('Adding job:', job)
       postJob({ body: job })
    }
    return (
